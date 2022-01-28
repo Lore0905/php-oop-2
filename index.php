048f5aa66a0fdcf968d87627ff1a837ef0300aea
Lore0905 <lore.castelli4@gmail.com>
2022-01-28 15:04:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,2 @@
+<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti. -->
